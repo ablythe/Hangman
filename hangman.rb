fbd8d805df9fae7457edccc5dd03c64bc4a5284e
Alexander Blythe <Alexander.blythe@gmail.com>
2015-01-28 15:43:36 -0500

    working version, no win/loss functionality

---
+++ hangman.rb
@@ -0,0 +1,47 @@
+require "pry"
+
+def hangman_initializer(lives, answer)
+  answer_length = answer.length
+  starting_lives = lives
+  guessed_letters = []
+  guess = "_ " * answer_length
+  hangman_player(starting_lives, guessed_letters, guess, answer)
+end
+
+def hangman_player lives, guessed_letters, guess, answer
+  letter = texter(lives, guess, guessed_letters)
+  guess = guess
+  if answer.include?(letter)
+    counter = 0
+    answer.each_char do |current_letter|
+      if current_letter == letter
+        guess[counter] = letter
+        counter += 2
+      else
+        counter += 2
+      end
+    end
+  else
+    lives -= 1
+  end
+  guessed_letters << letter
+  hangman_player(lives, guessed_letters, guess, answer)
+end
+
+
+def texter(life, guess, guessed_letters)
+  puts "Remaining lives #{life}"
+  puts ": #{guess}"
+  puts "Guessed Letters: #{guessed_letters}"
+  print "Guess a letter: "
+  letter = gets.chomp
+  if letter.downcase[/[a-z]/]
+    return letter
+  else
+    puts "Not a letter, please try again:"
+    texter(life, guess, guessed_letters)
+  end
+end
+
+
+hangman_initializer(5, "food")
